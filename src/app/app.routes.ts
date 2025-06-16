ba146ad49f00ed51e2b8908160c73e748f18ee65
Daniel <danielgilcastro@gmail.com>
2025-06-16 20:33:38 -0300

    primeiro commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { FormularioComponent } from './formulario/formulario.component';
+import { GaleriaComponent } from './galeria/galeria.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        component: FormularioComponent
+    },
+    {
+        path: "form",
+        component: FormularioComponent
+    },
+    {
+        path: "galeria",
+        component: GaleriaComponent
+    }
+];
